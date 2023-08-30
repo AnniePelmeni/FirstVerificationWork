469c495c4b1c5ab376b632a44498228fdc45e8f0
Anna <vitani2007@mail.ru>
2023-08-30 14:25:35 +0300

    Task solved

---
+++ Examination_task/Program.cs
@@ -0,0 +1,40 @@
+﻿string[] ChangeArray(string[] initialArray)
+{
+    int count = 0;
+    for(int i = 0; i < initialArray.Length; i++)
+    {
+        if (initialArray[i].Length < 4) count++;
+    }
+    string[] newArray = new string[count];
+    int k = 0;
+    int j = 0;
+    while (k < newArray.Length)
+    {
+        if (initialArray[j].Length < 4) 
+        {
+            newArray[k] = initialArray[j];
+            j++;
+            k++;
+        }
+        else j++;
+    }
+    return newArray;    
+}
+
+string[] FillArray()
+{
+    string[] array = new string[4];
+    for(int i = 0; i < 4; i++)
+    {
+        Console.WriteLine("Напишите слово: ");
+        array[i] = Console.ReadLine();
+    }
+    return array;
+}
+
+string[] array = FillArray();
+string str = string.Join(" ", array);
+Console.Write($"[{str}] -> ");
+string[] array2 = ChangeArray(array);
+string str2 = string.Join(" ", array2);
+Console.WriteLine($"[{str2}]");
